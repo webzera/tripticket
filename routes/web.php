e94e164e638468e42d4669feadf286041cab637b
webzera <esjohnzen@gmail.com>
2020-07-25 15:24:23 +0530

    Add Auth folder added

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Service Name 					URL
+
+// Search Service 					https://apitest.tripjack.com/fms/v1/air-search-all
+// FareRule Service 				https://apitest.tripjack.com/fms/v1/farerule
+// Review Service 					https://apitest.tripjack.com/fms/v1/review
+// Book Service (Instant Ticket)	https://apitest.tripjack.com/oms/v1/air/book
+// Book Service (Hold) 				https://apitest.tripjack.com/oms/v1/air/book
+// Confirm Fare Before Ticket 		https://apitest.tripjack.com/oms/v1/air/fare-validate
+// Book Service (Confirm Hold Book) https://apitest.tripjack.com/oms/v1/air/confirm-book
+// Booking Details 					https://apitest.tripjack.com/oms/v1/booking-details
+// Release PNR(Hold) 				https://apitest.tripjack.com/oms/v1/air/unhold
+// Seat Service 					https://apitest.tripjack.com/fms/v1/seat
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
